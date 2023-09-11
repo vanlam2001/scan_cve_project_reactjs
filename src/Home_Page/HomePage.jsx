b7528b430f103540eb4d344c87cbe26f039befb0
Van Lam Nguyen <vanlam1412@gmail.com>
2023-09-11 22:20:52 +0700

    update

--- src/Home_Page/HomePage.jsx
+++ src/Home_Page/HomePage.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Banner_Page from '../Components/Banner_Page/Banner_Page'
+
 
 const Home_Page = () => {
     return (
         <div>
-            <Banner_Page />
+
         </div>
     )
 }
