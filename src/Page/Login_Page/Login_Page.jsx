bdda26bfce9f46ad4cc8fdc719147449b60d658e
Van Lam Nguyen <vanlam1412@gmail.com>
2023-09-13 00:00:09 +0700

    update

--- src/Page/Login_Page/Login_Page.jsx
+++ src/Page/Login_Page/Login_Page.jsx
@@ -115,7 +115,7 @@ const Login_Page = () => {
                             </div>
                         </div>
                         <div className="w-full md:w-full px-3">
-                            <button className="appearance-none block w-full bg-blue-600 text-gray-100 font-bold border border-gray-200 rounded-lg py-3 px-3 leading-tight hover:bg-blue-500 focus:outline-none focus:bg-white focus:border-gray-500">Sign in</button>
+                            <button className="appearance-none block w-full bg-blue-600 text-gray-100 font-bold border border-gray-200 rounded-lg py-3 px-3 leading-tight hover:bg-blue-500 focus:outline-none ">Sign in</button>
                         </div>
                     </div>
                 </Form>
