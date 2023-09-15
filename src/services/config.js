bbb0b85baf111a8de85b746d78dd711aae587916
Van Lam Nguyen <vanlam1412@gmail.com>
2023-09-15 23:28:51 +0700

    update

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'https://api-cve-scan-project.onrender.com/';
+const baseUrl = 'http://127.0.0.1:8000/';
 const Token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
 
 const configHeader = () => {
