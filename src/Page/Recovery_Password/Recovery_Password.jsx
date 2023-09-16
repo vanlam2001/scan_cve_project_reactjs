37e00ec9dcc8bbf4ec36480a726783bd44d53ec0
vanlam <vanlam1412@gmail.com>
2023-09-17 02:46:48 +0700

    update

--- src/Page/Recovery_Password/Recovery_Password.jsx
+++ src/Page/Recovery_Password/Recovery_Password.jsx
@@ -1,18 +1,20 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { resetPassword } from '../../Toolkits/restPassword';
+import { message, Input, Button, Form } from 'antd';
+import { UserOutlined, LockOutlined, UserAddOutlined, MailOutlined, PhoneOutlined } from '@ant-design/icons';
 
 const Recovery_Password = () => {
     const dispatch = useDispatch();
-    const resetPasswordStatus = useSelector((state) => state.passwordReset.status)
-    const resetPasswordMessage = useSelector((state) => state.passwordReset.message)
+    const resetPasswordStatus = useSelector((state) => state.passwordReset.status);
+    const resetPasswordMessage = useSelector((state) => state.passwordReset.message);
 
     const [formData, setFormData] = useState({
         username: '',
         recovery_password: '',
         new_password: '',
         confirm_password: '',
-    })
+    });
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
@@ -20,50 +22,131 @@ const Recovery_Password = () => {
             ...formData,
             [name]: value,
         });
-    }
+    };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    const handleSubmit = () => {
         dispatch(resetPassword(formData));
     };
+
     return (
         <div>
-            <h2>Password Reset</h2>
             {resetPasswordStatus === 'succeeded' && <div>{resetPasswordMessage}</div>}
             {resetPasswordStatus === 'failed' && <div>Error: {resetPasswordMessage}</div>}
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    name="username"
-                    placeholder="Username"
-                    value={formData.username}
-                    onChange={handleInputChange}
-                />
-                <input
-                    type="text"
-                    name="recovery_password"
-                    placeholder="Recovery Password"
-                    value={formData.recovery_password}
-                    onChange={handleInputChange}
-                />
-                <input
-                    type="password"
-                    name="new_password"
-                    placeholder="New Password"
-                    value={formData.new_password}
-                    onChange={handleInputChange}
-                />
-                <input
-                    type="password"
-                    name="confirm_password"
-                    placeholder="Confirm Password"
-                    value={formData.confirm_password}
-                    onChange={handleInputChange}
-                />
-                <button type="submit">Reset Password</button>
-            </form>
-        </div>
-    )
-}
+            <div className="text-center mt-24">
+                {/* component */}
+                <div className="flex items-center justify-center">
+                    <svg
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        class="w-12 h-12"
+                        stroke="currentColor"
+                    >
+                        <image href="https://www.emojimeaning.com/img/img-apple-160/1f1fb-1f1f3.png" x="0" y="0" width="24" height="24" />
+                    </svg>
+                </div>
+                <h2 className="text-4xl tracking-tight">Lấy lại mật khẩu</h2>
+            </div>
+            <div className="flex justify-center my-2 mx-4 md:mx-0">
+                <Form
+                    onFinish={handleSubmit}
+                    name="register"
+                    initialValues={{
+                        remember: true,
+                    }}
 
-export default Recovery_Password
+                    className="w-full max-w-xl bg-white rounded-lg shadow-md p-6"
+                >
+                    <Form.Item
+                        name="username"
+                        label="Tên người dùng"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Vui lòng nhập tên người dùng',
+                            },
+                        ]}
+                    >
+                        <Input prefix={<UserOutlined />}
+                            placeholder='Tên người dùng' />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="recovery_password"
+                        label="Mật khẩu cấp 2"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Vui lòng nhập mật khẩu cấp 2!',
+                            },
+                        ]}
+                        hasFeedback
+                    >
+                        <Input.Password
+                            prefix={<LockOutlined />}
+                            placeholder="Password"
+                        />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="new_password"
+                        label="Mật khẩu mới"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Vui lòng nhập mật khẩu mới!',
+                            },
+                        ]}
+                        hasFeedback
+                    >
+                        <Input.Password
+                            prefix={<LockOutlined />}
+                            placeholder="Password"
+                        />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="confirm_password"
+                        label="Nhập lại mật khẩu"
+                        dependencies={['new_password']}
+                        hasFeedback
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Vui lòng nhập mật khẩu!',
+                            },
+                            ({ getFieldValue }) => ({
+                                validator(_, value) {
+                                    if (!value || getFieldValue('new_password') === value) {
+                                        return Promise.resolve();
+                                    }
+                                    return Promise.reject(
+                                        new Error('The two passwords do not match!')
+                                    );
+                                },
+                            }),
+                        ]}
+                    >
+                        <Input.Password
+                            prefix={<LockOutlined />}
+                            placeholder="Confirm Password"
+                        />
+                    </Form.Item>
+
+
+
+                    <Form.Item>
+                        <button
+                            type="primary"
+                            htmlType="submit"
+                            className="appearance-none block w-full bg-blue-600 text-gray-100 font-bold border border-gray-200 rounded-lg py-3 px-3 leading-tight hover:bg-blue-500 focus:outline-none "
+                        >
+                            Xác nhận
+                        </button>
+                    </Form.Item>
+                </Form>
+            </div>
+        </div>
+    );
+};
+
+export default Recovery_Password;
